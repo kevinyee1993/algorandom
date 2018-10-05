2e431521e5192f6ab63ba0e681f1f575043f5a07
Kevin Yee <kevinyee1993@gmail.com>
2018-10-05 03:03:28 -0700

    Set up react and redux

---
+++ src/reducers/root_reducer.jsx
@@ -0,0 +1,9 @@
+import { combineReducers } from 'redux';
+
+// just leaving this in for now to avoid getting warning but can
+// take away later once you create a real reducer
+import StartingReducer from './starting_reducer';
+
+export default combineReducers({
+  StartingReducer
+});
